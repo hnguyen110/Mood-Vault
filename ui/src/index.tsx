c575071146f6218fbabefb6bb7cd129ed7f170ec
Hien Nguyen <hnguyen110.akc@gmail.com>
2023-04-28 00:00:00 -0400

    Added front-end dependencies

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material";
+import { indigo } from "@mui/material/colors";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={createTheme({
+        palette: {
+          primary: indigo,
+          secondary: indigo,
+        },
+      })}
+    >
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
